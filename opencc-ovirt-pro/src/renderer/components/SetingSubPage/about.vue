b235ca0e1b12ac0e9cff7d30c79b25787a08df7e
wang_song <zero_ws@163.com>
2020-11-18 17:37:12 +0800

    更新客户端关于窗口信息

--- opencc-ovirt-pro/src/renderer/components/SetingSubPage/about.vue
+++ opencc-ovirt-pro/src/renderer/components/SetingSubPage/about.vue
@@ -1,8 +1,14 @@
 <template>
   <div style="padding-top:20px ;padding-left:40px">
-    <div class="about-text">产品名称 : 开源OVIRT客户端</div>
-    <br>
+    <div class="about-text">产品名称 : OpenCC</div>
+    <br />
     <div class="about-text">版 本 号 : {{ editionNum }}</div>
+    <br />
+    <div class="about-text">描 述：开源oVirt云桌面客户端</div>
+    <br />
+    <div class="about-text">支 持：oVirt中文社区（www.cnovirt.com）</div>
+    <br />
+    <div class="about-text">授权协议：MIT</div>
   </div>
 </template>
 
@@ -11,7 +17,7 @@ export default {
   name: 'seting-about-page',
   data() {
     return {
-      editionNum: 'V1.0.0.20200604',
+      editionNum: 'V2.0.1.20201118',
     }
   },
 }
